fc3f03e2cd766515054cf68ecd5fae7d7aaa1ba9
dogukansitil <dogukansitil@gmail.com>
2023-11-17 14:56:02 +0300

    Multi language support and some features

--- CPark.h
+++ CPark.h
@@ -1,22 +1,94 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+﻿#include <iostream>
 #include <string>
-#include <ctime>
-#include <stdlib.h>
 #include <fstream>
-
+#include <sstream>
+#include <ctime>
+#include <conio.h>
+#include <map> 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #ifdef _WIN64
-//64 Bit
 #include <windows.h>
 #else
-//32 Bit
 #include <windows.h>
 #endif
 #elif __linux__
 #include <unistd.h>
+#elif TARGET_OS_MAC
+
 #endif
-
-
-
 using namespace std;
+
+#define AccountDataFile "AccountData.txt"
+int FailedLoginCounter = 0;
+bool bIsLoggedIn = false;
+int State = 0;
+double HourlyRate = 5.0;
+
+
+struct Accounts {
+    string Username;
+    string Password;
+    string Name;
+    string Surname;
+} CurrentAccount;
+
+struct CarPark {
+    bool bIsFull = false;
+    string Plate = "";
+    tm Time;
+}CurrentCarParks[5][10];
+
+void ShowCarParkList();
+void ParkNewCar();
+void ParkOutCar();
+double CalculateParkingFee(const tm* entryTime, const tm* exitTime, double hourlyRate);
+
+map<string, string> translations;
+
+
+void LoadTranslations(string currentLanguage) {
+    string translationFilePath = "lang/translations_" + currentLanguage + ".txt";
+    ifstream translationFile(translationFilePath);
+
+    if (!translationFile) {
+        cout << "Translation file not found for language " << currentLanguage << ". Using default (English)." << endl;
+        return;
+    }
+
+    string line;
+    while (getline(translationFile, line)) {
+        size_t equalSignPos = line.find('=');
+        if (equalSignPos != string::npos) {
+            string key = line.substr(0, equalSignPos);
+            string value = line.substr(equalSignPos + 1);
+            translations[key] = value;
+        }
+    }
+
+    translationFile.close();
+}
+
+
+string Translate(string text) {
+    
+    auto it = translations.find(text);
+    if (it != translations.end()) {
+        return it->second;
+    }
+    
+    return text;
+}
+
+double CalculateParkingFee(tm* const entryTime, tm* const exitTime, double HourlyRate) {
+    
+    time_t entryTimestamp = mktime(entryTime);
+    time_t exitTimestamp = mktime(exitTime);
+    double timeDifference = difftime(exitTimestamp, entryTimestamp);
+    
+    double hours = timeDifference / 3600.0;
+
+    
+    double fee = HourlyRate * ceil(hours);
+
+    return fee;
+}
--- CPark_Console _App.cpp
+++ CPark_Console _App.cpp
@@ -1,204 +1,390 @@
-#include "CPark.h"
+ï»¿#include "CPark.h"
 
-//K.Adi , Sifre , Max Yanlis Giris Hakki, Otoparkin kaç katli olacagi ve her katta kaç araç olacagi gibi degiskenler tanimlandi
-const string adminUsername = "Admin";
-const string adminPassword = "Admin";
-const int maxLoginAttempts = 3;
-const int floors = 5;
-const int spotsPerFloor = 10;
-
-//Araç girisinde programin kayit edecegi veriler struct ile tanimlandi
-struct Car {
-    string plate;
-    time_t entryTime;
-};
-//Programa 5 Kat ve her katta 10 araç olacagini belirten matrix tanimlandi
-struct ParkingLot {
-    Car cars[5][10];
-};
-//Karsilama ekrani yaziari
-void welcomeScreen() {
-    cout << "Hos geldiniz!" << endl;
-}
-//K.adi ve Sifre dogrulamasinin yapildigi fonksiyon while loop ve if kullanildi
-bool login() {
-    string username, password;
-    int loginAttempts = 0;
-    //3 Giristen az hatali islem yapildiysa kullanici adi ve Sifre girmemize izin veren ekran
-    while (loginAttempts < maxLoginAttempts) {
-        cout << "Kullanici Adi: ";
-        cin >> username;
-        cout << "Sifre: ";
-        cin >> password;
-        //Eger K.adi ve Sifre yukarida belirtilen ile ayni ise true döner
-        if (username == adminUsername && password == adminPassword) {
-            return true;
-        }
-        //Eger K.adi ve Sifre yukarida belirtilen ile ayni degil ise uyari verip tekrar giris yapmamizi ister
-        else {
-            loginAttempts++;
-            cout << "Yanlis giris yaptiniz. Tekrar deneyiniz. Toplam 3 hatali giristen sonra program kapanacaktir " << endl;
-        }
-    }
-    //Eger K.adi ve Sifre yukarida belirtilen ile ayni ise false döner
-    return false;
-}
-//Programi derlerken online bir editör kullandigim için CLS fonksiyonlarini çalistiramadim bu yüzden ekrani temizletmek için Stackoverflowdan buldugum bu fonsiyonu kullandim
-void clearScreen() {
-    cout << string(100, '\n');
-}
-//Basarili giris ardindan bize programimizin 4 tane çalisan fonksiyonundan hangisini seçmemizi istedigi ekran 
-void displayMenu() {
-    cout << "1. Park Halindeki Araclarin Listesi" << endl;
-    cout << "2. Park Halindeki Arac Cikisi" << endl;
-    cout << "3. Yeni Arac Girisi" << endl;
-    cout << "4. Cikisi" << endl;
-}
-//Otoparkta mevcut bütün araçlar?n listesini veren fonksiyonumuz
-void displayCars(const ParkingLot& parkingLot) {
-    for (int floor = 0; floor < floors; floor++) {
-        for (int spot = 0; spot < spotsPerFloor; spot++) {
-            if (!parkingLot.cars[floor][spot].plate.empty()) {
-                cout << "Kat " << floor + 1 << ", Park Yeri " << spot + 1 << " - Plaka: " << parkingLot.cars[floor][spot].plate << " Park Halinde Giris: " << ctime(&parkingLot.cars[floor][spot].entryTime);
-            }
-        }
-    }
-}
-//2. Park Halindeki Araç Çikisi fonksiyonuna girildiginde izlenecek yol, eger bos ise aracin plakasinin ve girisin yapildigi saatin kaydi, eger dolu ise hata ekrani ve displayMenu'ye dönüs
-void parkCar(ParkingLot& parkingLot) {
-    int floor, spot;
-    cout << "Kat numarasini girin: ";
-    cin >> floor;
-    floor--;
-
-    if (floor < 0 || floor >= floors) {
-        cout << "Gecersiz kat numarasi!" << endl;
-        return;
-    }
-    //Kattaki tüm slotlarin dolu olmasi durumunda bu kat dolu hatasini yoklayan fonksiyon
-    bool isFull = true;
-    for (int spot = 0; spot < spotsPerFloor; spot++) {
-        if (parkingLot.cars[floor][spot].plate.empty()) {
-            isFull = false;
-            break;
-        }
-    }
-    
-    if (isFull) {
-        cout << "Bu katta bos park yeri kalmamistir!" << endl;
-        Sleep(200);
-        return;
-    }
-
-    cout << "Park yeri numarasini girin: ";
-    cin >> spot;
-    spot--;
-
-    if (spot < 0 || spot >= spotsPerFloor) {
-        cout << "Gecersiz park yeri numarasi!" << endl;
-        return;
-    }
-
-    if (!parkingLot.cars[floor][spot].plate.empty()) {
-        cout << "Bu park yeri dolu!" << endl;
-        return;
-    }
-
-    cout << "Plaka: ";
-    cin >> parkingLot.cars[floor][spot].plate;
-    parkingLot.cars[floor][spot].entryTime = time(0);
-}
-//3.Seçenek araç çikisi yapan fonksiyon
-void exitCar(ParkingLot& parkingLot) {
-    int floor, spot;
-    cout << "Kat numarasini girin: ";
-    cin >> floor;
-    floor--;
-
-    if (floor < 0 || floor >= floors) {
-        cout << "Gecersiz kat numarasi!" << endl;
-        return;
-    }
-
-    cout << "Cikis yapilacak park yeri numarasini girin: ";
-    cin >> spot;
-    spot--;
-
-    if (spot < 0 || spot >= spotsPerFloor) {
-        cout << "Gecersiz park yeri numarasi!" << endl;
-        return;
-    }
-
-    if (parkingLot.cars[floor][spot].plate.empty()) {
-        cout << "Bu park yeri bos!" << endl;
-        return;
-    }
-    //Aracin giris yaptigi zaman ile suanki zaman arasindaki farki saptamak için time_t kullanildi
-    time_t currentTime = time(0);
-    double duration = difftime(currentTime, parkingLot.cars[floor][spot].entryTime);
-
-    cout << "Arac plakas?: " << parkingLot.cars[floor][spot].plate << endl;
-    cout << "Toplam park suresi: " << duration << " saniye" << endl;
-    cout << "Ucret: " << duration * 0.1 << " TL" << endl;
-    //Park yerinin temizlendigi yer
-    parkingLot.cars[floor][spot] = {};  
-        cout << "Arac cikisi tamamlandi." << endl;
-}
-
-//Otoparkin bos bir sekilde acilip sirasiyla fonksiyonlari islettigimiz ekran
 int main() {
-    ParkingLot parkingLot = {};
 
-    while (true) {
-        clearScreen();
-        welcomeScreen();
-        if (!login()) {
-            return 0;
-        }
+#ifdef _WIN32
+	SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+#endif
 
-        int choice;
+	string currentLanguage;
+	cout << "Please select a language (en, tr, de, es, fr): ";
+	cin >> currentLanguage;
+	LoadTranslations(currentLanguage);
 
-        while (true) {
-            clearScreen();
-            cout << "Hos geldiniz, " << adminUsername << endl;
-            displayMenu();
-            cout << "Seciminizi yapin: ";
-            cin >> choice;
+	ifstream AccountData;
 
-            switch (choice) {
-            case 1:
-                clearScreen();
-                cout << "Park Halindeki Araclar:" << endl;
-                displayCars(parkingLot);
-                cin.ignore();
-                cout << "Ana menuye donmek icin Enter tusuna basin...";
-                cin.get();
-                break;
-            case 2:
-                clearScreen();
-                cout << "Park Yeri Cikisi:" << endl;
-                exitCar(parkingLot);
-                cin.ignore();
-                cout << "Park cikisi basari ile yapildi. Ana menuye donmek icin Enter tusuna basin...";
-                cin.get();
-                break;
-            case 3:
-                clearScreen();
-                cout << "Yeni Arac Girisi:" << endl;
-                parkCar(parkingLot);
-                cin.ignore();
-                cout << "Arac girisi basari ile yapildi. Ana menuye donmek icin Enter tusuna basin...";
-                cin.get();
-                break;
-            case 4:
-                cin.ignore();
-                cout << "Programdan cikis yapiliyor iyi gunler...";
-                cin.get();
-                return 0;
-            default:
-                cout << "Gecersiz secenek!" << endl;
-            }
-        }
-    }
-    return 0;
+	while (FailedLoginCounter != 3 && bIsLoggedIn == false) {
+		system("CLS");
+		AccountData.open(AccountDataFile);
+		if (!AccountData) {
+			cout << Translate("database_error");
+			return -1;
+		}
+		
+		string Username = "";
+		string Password = "";
+		string ReadingAccount = "";
+		string ReadingUsername = "";
+		string ReadingPassword = "";
+		string ReadingName = "";
+		string ReadingSurname;
+		
+		
+
+		cout << Translate("welcome_message") << endl << Translate("enter_credientals") << endl;
+		cout << Translate("enter_username");
+		cin >> Username;
+		cout << Translate("enter_password");
+		cin >> Password;
+		getline(AccountData, ReadingAccount);
+		stringstream Splitter(ReadingAccount);
+		Splitter >> ReadingUsername >> ReadingPassword >> ReadingName >> ReadingSurname;
+		if (Username == ReadingUsername && Password == ReadingPassword) {
+			CurrentAccount.Username = ReadingUsername;
+			CurrentAccount.Password = ReadingPassword;
+			CurrentAccount.Name = ReadingName;
+			CurrentAccount.Surname = ReadingSurname;
+			bIsLoggedIn = true;
+			system("CLS");
+			cout << Translate("login_succesfull");
+			Sleep(500);
+			system("CLS");
+			cout << Translate("login_successful") <<".";
+			Sleep(500);
+			system("CLS");
+			cout << Translate("login_successful") <<".." ;
+			Sleep(500);
+			system("CLS");
+		}
+		else {
+			FailedLoginCounter++;
+			system("CLS");
+			cout << Translate("login_failed") << endl;
+			if ((3 - FailedLoginCounter) > 0) {
+				cout << Translate("remaining_attempts") << (3 - FailedLoginCounter);
+			}
+			else {
+				cout << Translate("no_attempts_left");
+			}
+			Sleep(2500);
+			system("CLS");
+
+		}
+		AccountData.close();
+	}
+
+	while (bIsLoggedIn == true && State != 4) {
+		system("CLS");
+		cout << Translate("welcome_message") << CurrentAccount.Name << " " << CurrentAccount.Surname << endl;
+		cout << Translate("list_cars") << endl;
+		cout << Translate("new_car_entry") << endl;
+		cout << Translate("car_exit") << endl;
+		cout << Translate("exit_program") << endl;
+		cout << Translate("select_option");
+		cin >> State;
+		switch (State) {
+		case 1:
+			ShowCarParkList();
+			break;
+		case 2:
+			ParkNewCar();
+			break;
+		case 3:
+			ParkOutCar();
+			break;
+		case 4:
+			return 0;
+			break;
+		default:
+			system("CLS");
+			cout << Translate("invalid_option");
+			Sleep(2500);
+			system("CLS");
+			cout << Translate("wait_main_menu");
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << ".";
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << "..";
+			Sleep(500);
+			system("CLS");
+			break;
+		}
+	}
+
+	system("PAUSE");
+	return 0;
+}
+
+void ShowCarParkList() {
+	system("CLS");
+	int FloorNumber = 0;
+	bool bIsAllEmpty = true;
+	for (int i = 0; i < 5; i++) {
+		for (int x = 0; x < 10; x++) {
+			if (CurrentCarParks[i][x].bIsFull == true) {
+				bIsAllEmpty = false;
+			}
+		}
+	}
+	if (bIsAllEmpty == true) {
+		system("CLS");
+		cout << Translate("empty_carpark");
+		Sleep(2500);
+		system("CLS");
+		cout << Translate("wait_main_menu");
+		Sleep(500);
+		system("CLS");
+		cout << Translate("wait_main_menu") << ".";
+		Sleep(500);
+		system("CLS");
+		cout << Translate("wait_main_menu") << "..";
+		Sleep(500);
+		system("CLS");
+	}
+	else {
+		cout << Translate("list_floor_number");
+		cin >> FloorNumber;
+		if (FloorNumber > 0 && FloorNumber < 6) {
+			bIsAllEmpty = true;
+			for (int i = 0; i < 10; i++) {
+				if (CurrentCarParks[FloorNumber - 1][i].bIsFull == true) {
+					bIsAllEmpty = false;
+				}
+			}
+			if (bIsAllEmpty) {
+				system("CLS");
+				cout << FloorNumber << Translate("park_empty_floor");
+				Sleep(2500);
+				system("CLS");
+				cout << Translate("wait_main_menu");
+				Sleep(500);
+				system("CLS");
+				cout << Translate("wait_main_menu") << ".";
+				Sleep(500);
+				system("CLS");
+				cout << Translate("wait_main_menu") << "..";
+				Sleep(500);
+				system("CLS");
+			}
+			else {
+				cout << endl;
+				for (int i = 0; i < 10; i++) {
+					cout << FloorNumber << Translate("floor") << i + 1 << Translate("park_place") << CurrentCarParks[FloorNumber - 1][i].Plate << endl;
+					int Year = CurrentCarParks[FloorNumber - 1][i].Time.tm_year + 1900;
+					int Month = CurrentCarParks[FloorNumber - 1][i].Time.tm_mon + 1;
+					int Day = CurrentCarParks[FloorNumber - 1][i].Time.tm_mday;
+					int Hour = CurrentCarParks[FloorNumber - 1][i].Time.tm_hour;
+					int Minute = CurrentCarParks[FloorNumber - 1][i].Time.tm_min;
+					int Second = CurrentCarParks[FloorNumber - 1][i].Time.tm_sec;
+					cout << Translate("car_entry_date") << Day << ":" << Month << ":" << Year << " " << Hour << ":" << Minute << ":" << Second << endl;
+				}
+				cout << endl << Translate("return_main_menu") << endl;
+				_getch();
+			}
+		}
+		else {
+			system("CLS");
+			cout << Translate("invalid_floor_number");
+			Sleep(2500);
+			system("CLS");
+			cout << Translate("wait_main_menu");
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << ".";
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << "..";
+			Sleep(500);
+			system("CLS");
+		}
+	}
+}
+
+void ParkNewCar() {
+	system("CLS");
+	int FloorNumber = 0;
+	cout << Translate("select_floor_number");
+	cin >> FloorNumber;
+	bool bHasAnyParkEmpty = false;
+	string Plate = "";
+	int ParkNumber = 0;
+	system("CLS");
+	cout << Translate("park_list_title") << endl;
+	for (int i = 0; i < 10; i++) {
+		if (CurrentCarParks[FloorNumber - 1][i].bIsFull == false) {
+			cout << i + 1 << Translate("park_place_available") << endl;
+			bHasAnyParkEmpty = true;
+		}
+		else {
+			cout << i + 1 << Translate("park_place_not_available") << endl;
+		}
+	}
+	if (bHasAnyParkEmpty == true) {
+		cout << endl;
+		cout << Translate("car_information") << endl;
+		cout << Translate("park_place_num");
+		cin >> ParkNumber;
+		cout << Translate("plate");
+		cin >> Plate;
+		if (ParkNumber > 0 && ParkNumber < 11) {
+			if (CurrentCarParks[FloorNumber - 1][ParkNumber - 1].bIsFull == true) {
+				system("CLS");
+				cout << ParkNumber << Translate("park_place_not_available");
+				Sleep(2500);
+				system("CLS");
+				cout << Translate("wait_main_menu");
+				Sleep(500);
+				system("CLS");
+				cout << Translate("wait_main_menu") << ".";
+				Sleep(500);
+				system("CLS");
+				cout << Translate("wait_main_menu") << "..";
+				Sleep(500);
+				system("CLS");
+			}
+			else {
+				time_t Now = time(nullptr);
+				localtime_s(&CurrentCarParks[FloorNumber - 1][ParkNumber - 1].Time, &Now);
+				CurrentCarParks[FloorNumber - 1][ParkNumber - 1].bIsFull = true;
+				CurrentCarParks[FloorNumber - 1][ParkNumber - 1].Plate = Plate;
+				system("CLS");
+				cout << Plate << Translate("car") << " "  << FloorNumber << "." << Translate("floor") << ParkNumber << Translate("parked_location") << endl;
+				Sleep(2500);
+				system("CLS");
+				cout << Translate("wait_main_menu");
+				Sleep(500);
+				system("CLS");
+				cout << Translate("wait_main_menu") << ".";
+				Sleep(500);
+				system("CLS");
+				cout << Translate("wait_main_menu") << "..";
+				Sleep(500);
+				system("CLS");
+			}
+		}
+		else {
+			system("CLS");
+			cout << Translate("invalid_park_place");
+			Sleep(2500);
+			system("CLS");
+			cout << Translate("wait_main_menu");
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << ".";
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << "..";
+			Sleep(500);
+			system("CLS");
+		}
+	}
+	else {
+		system("CLS");
+		cout << FloorNumber << "." << Translate("park_full_floor");
+		Sleep(2500);
+		system("CLS");
+		cout << Translate("wait_main_menu");
+		Sleep(500);
+		system("CLS");
+		cout << Translate("wait_main_menu") << ".";
+		Sleep(500);
+		system("CLS");
+		cout << Translate("wait_main_menu") << "..";
+		Sleep(500);
+		system("CLS");
+	}
+}
+
+void ParkOutCar() {
+	system("CLS");
+	string Plate;
+	int SelectedFloorNumber = 0;
+	int SelectedParkNumber = 0;
+	bool bIsFoundCar = false;
+	cout << Translate("car_exit_plate");
+	cin >> Plate;
+	if (Plate.length() > 0) {
+		for (int i = 0; i < 5; i++) {
+			for (int x = 0; x < 10; x++) {
+				if (CurrentCarParks[i][x].Plate == Plate) {
+					SelectedFloorNumber = i;
+					SelectedParkNumber = x;
+					bIsFoundCar = true;
+				}
+			}
+		}
+		if (bIsFoundCar == true) {
+			system("CLS");
+
+
+			time_t Now = time(nullptr);
+			tm OutTime;
+			tm* EmptyTime = new tm;
+			
+			localtime_s(&OutTime, &Now);
+			int Year = OutTime.tm_year + 1900;
+			int Month = OutTime.tm_mon + 1;
+			int Day = OutTime.tm_mday;
+			int Hour = OutTime.tm_hour;
+			int Minute = OutTime.tm_min;
+			int Second = OutTime.tm_sec;
+			int InYear = CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time.tm_year + 1900;
+			int InMonth = CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time.tm_mon + 1;
+			int InDay = CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time.tm_mday;
+			int InHour = CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time.tm_hour;
+			int InMinute = CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time.tm_min;
+			int InSecond = CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time.tm_sec;
+			cout << CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Plate << Translate("car_discard") << endl;
+			cout << Translate("car_entry_date") << InDay << ":" << InMonth << ":" << InYear << " " << InHour << ":" << InMinute << ":" << InSecond << endl;
+			cout << Translate("car_exit_date") << Day << ":" << Month << ":" << Year << " " << Hour << ":" << Minute << ":" << Second << endl;
+			double fee = CalculateParkingFee(&CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time, &OutTime, HourlyRate);
+			cout << Translate("price") <<  fee  << Translate("currency");
+			CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].bIsFull = false;
+			CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Plate = "";
+			CurrentCarParks[SelectedFloorNumber][SelectedParkNumber].Time = *EmptyTime;
+			Sleep(5000);
+			system("CLS");
+			cout << Translate("wait_main_menu");
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << ".";
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << "..";
+			Sleep(500);
+			system("CLS");
+		}
+		else {
+			system("CLS");
+			cout << Translate("plate_not_found");
+			Sleep(2500);
+			system("CLS");
+			cout << Translate("wait_main_menu");
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << ".";
+			Sleep(500);
+			system("CLS");
+			cout << Translate("wait_main_menu") << "..";
+			Sleep(500);
+			system("CLS");
+		}
+	}
+	else {
+		system("CLS");
+		cout << Translate("invalid_plate");
+		Sleep(2500);
+		system("CLS");
+		cout << Translate("wait_main_menu");
+		Sleep(500);
+		system("CLS");
+		cout << Translate("wait_main_menu") << ".";
+		Sleep(500);
+		system("CLS");
+		cout << Translate("wait_main_menu") << "..";
+		Sleep(500);
+		system("CLS");
+	}
 }
